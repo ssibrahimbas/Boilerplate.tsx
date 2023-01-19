e6b27e7d01b8edacc7268500ce0dc443e7c5fd28
Sami Salih İbrahimbas <info@ssibrahimbas.com>
2023-01-19 21:25:59 +0300

    ci: test linting

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React from "react"
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
-        <h1 className="text-3xl font-bold">
-            Hello world!
-        </h1>
-    </div>
-  )
+	return (
+		<div className="App">
+			<h1 className="text-3xl font-bold">Hello world!</h1>
+		</div>
+	)
 }
 
 export default App
